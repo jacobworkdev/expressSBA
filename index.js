407f130e9e6b6065a38740bd32d241122c38972a
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-01-24 14:27:01 -0600

    user and post route to test and practice

--- index.js
+++ index.js
@@ -2,12 +2,41 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const userRoutes = require("./routes/user");
-app.use("/user", userRoutes);
+// Importing the data from our fake database files.
+const users = require("./data/users");
+const posts = require("./data/posts");
 
 app.get("/", (req, res) => {
-    res.send("Keeping it simple.");
+    res.send("Work in progress!");
 });
+
+app.get('/api/users', (req, res) => {
+    res.json(users)
+})
+app.get('/api/users/:id', (req, res, next) => {
+    const user = users.find((u) => u.id == req.params.id);
+    if (user) res.json(user);
+    else next()
+})
+
+
+
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts)
+})
+app.get('/api/posts/:id', (req, res) => {
+    const post = posts.find((p) => p.id == req.params.id);
+    if (post) res.json(post);
+})
+
+
+app.use((req, res) => {
+    res.status(404);
+    res.json({ error: "Resource Not Found" });
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}.`);
