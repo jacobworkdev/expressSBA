73199992f9f8f24a021fdde4b756b3d22068e3ad
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-01-31 03:52:28 -0600

    comment id with uuid and show route implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {v4:uuid}=require('uuid')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -8,15 +9,17 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 const comments = [
-    {
+    {   id:uuid(),
         user: 'jacob',
         comment: 'Hello i just got a swe job'
     },
-    {
+    {   
+        id:uuid(),
         user: 'jordan',
         comment: 'NY is the best city'
     },
     {
+        id:uuid(),
         user: 'bryan',
         comment: 'talk to me man'
     }
@@ -31,10 +34,18 @@ app.get('/comments/new',(req,res)=>{
 })
 
 app.post('/comments',(req,res)=>{
-    comments.push({user:req.body.user,comment:req.body.comment})
+    comments.push({id:uuid(),user:req.body.user,comment:req.body.comment})
     res.redirect('/comments')
 })
 
+//get based on comment id
+app.get('/comments/:id',(req,res)=>{
+    const id = req.params.id
+    const comment = comments.find(c=>c.id===id)
+    res.render('comments/show',{comment})
+
+
+})
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
